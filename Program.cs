d58f3e6d67238f3210aa469084217f913ae590b6
badmus sulaiman kola <31166533+badmusk@users.noreply.github.com>
2020-09-01 12:20:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Exercise33
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput integer:");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        for (x = 1; x <100; x++)
+
+
+
+        {
+            if (x % 3==0)
+                {
+
+                Console.WriteLine("multiple of 3 : {0}", x);
+
+            }
+
+        }
+    }
+}
